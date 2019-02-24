edf361ddfcc524d7ab1abdd32fdab6790659aac7
Owen Craston <owen.craston@shopify.com>
2019-02-23 20:36:51 -0500

    move ant in random direction when the spider moves

--- a1/spider_game.py
+++ a1/spider_game.py
@@ -26,63 +26,73 @@ def spawn_ant(border_choice):
     else:
         raise ValueError('border_choice must be an int value between 0-3.' + border_choice + ' was found')
 
+def handle_ant_movement(ant):
+    direction = randint(0,3)
+    if direction == 0:
+        ant[1] += 1
+    if direction == 1:
+        ant[0] += 1
+    if direction == 2:
+        ant[1] -=1
+    if direction == 3:
+        ant[0] -= 1
+    return ant
+
+
 
 border_choice = randint(0,3)
 ant = spawn_ant(border_choice)
 # add the ant to the screen
 w.addch(ant[0], ant[1], curses.ACS_DIAMOND)
+ant_direction = randint(0,3)
 
 # spider
 spider = [15, 35] # initial position values
 # add the spider to the screen
 w.addch(spider[0], spider[1], curses.ACS_PI)
 
-key = KEY_RIGHT
+# key = KEY_RIGHT
 score = 0
 
-while key != 27:
-    w.timeout(150)
+while w.getch() != 27:
+    w.timeout(50)
     w.border(0)
     w.addstr(0, 2, 'Score : ' + str(score) + ' ')
-    # get the next movement 
-    prevKey = key
-    nextKey = w.getch()
-    key = key if nextKey == -1 else nextKey
 
-    # logic for paused game
-    if key == ord(' '):                                            
-        while key != ord(' '):
-            key = w.getch()
-        key = prevKey
-        continue
-
-    # hanle invalid keys (anything but the arrow keys and escape key)
-    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:
-        key = prevKey
-
-    # remove the previous spider
-    w.addch(spider[0], spider[1], ' ')
     # handle spider movement
-    if key == KEY_RIGHT:
-        spider[1] += 1
-    if key == KEY_DOWN:
-        spider[0] += 1
-    if key == KEY_LEFT:
-        spider[1] -= 1
-    if key == KEY_UP:
+    if w.getch() == KEY_RIGHT:
+        # remove the previous spider
+        w.addch(spider[0], spider[1], ' ')
+        # remove the previous ant
+        w.addch(ant[0], ant[1], ' ')
+        spider[1] += 2
         spider[0] -= 1
-
-    # remove the previous ant
-    w.addch(ant[0], ant[1], ' ')
-    # increment coordinates for ant
-    if border_choice == 0:
-        ant[1] += 1
-    if border_choice == 1:
-        ant[0] += 1
-    if border_choice == 2:
-        ant[1] -=1
-    if border_choice == 3:
-        ant[0] -= 1
+        # move the ant randomlly
+        ant = handle_ant_movement(ant)
+    if w.getch() == KEY_DOWN:
+        # remove the previous spider
+        w.addch(spider[0], spider[1], ' ')
+        # remove the previous ant
+        w.addch(ant[0], ant[1], ' ')
+        spider[0] += 1
+        # move the ant randomlly
+        ant = handle_ant_movement(ant)
+    if w.getch() == KEY_LEFT:
+        # remove the previous spider
+        w.addch(spider[0], spider[1], ' ')
+        # remove the previous ant
+        w.addch(ant[0], ant[1], ' ')
+        spider[1] -= 1
+        # move the ant randomlly
+        ant = handle_ant_movement(ant)
+    if w.getch() == KEY_UP:
+        # remove the previous spider
+        w.addch(spider[0], spider[1], ' ')
+        # remove the previous ant
+        w.addch(ant[0], ant[1], ' ')
+        spider[0] -= 1
+        # move the ant randomlly
+        ant = handle_ant_movement(ant)
 
     # if the ant goes out of bounds 
     if ant[0] >= 30:
