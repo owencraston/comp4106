0e28a97d187b7fe37008b0bed7d45c066bf38f75
Owen Craston <owen.craston@shopify.com>
2019-02-25 17:10:00 -0500

    wip bfs algo
    - got some help and this is the state it was left in

--- a1/spider_game_ai.py
+++ a1/spider_game_ai.py
@@ -3,15 +3,15 @@ from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint
 
 # init game
-screen = curses.initscr()
-curses.curs_set(0)
-h = 30
-w = 70
-x, y = 0, 0
-w = curses.newwin(h, w, x, y)
-w.keypad(1)
-w.border(0)
-w.nodelay(1)
+# screen = curses.initscr()
+# curses.curs_set(0)
+# h = 30
+# w = 70
+# x, y = 0, 0
+# w = curses.newwin(h, w, x, y)
+# w.keypad(1)
+# w.border(0)
+# w.nodelay(1)
 
 
 def spawn_ant(border_choice):
@@ -41,18 +41,19 @@ def handle_ant_movement(direction, ant):
 border_choice = randint(0,3)
 ant = spawn_ant(border_choice)
 # add the ant to the screen
-w.addch(ant[0], ant[1], curses.ACS_DIAMOND)
+# w.addch(ant[0], ant[1], curses.ACS_DIAMOND)
 ant_direction = randint(0,3)
 
 # spider
 spider = [15, 35] # initial position values
 # add the spider to the screen
-w.addch(spider[0], spider[1], curses.ACS_PI)
+# w.addch(spider[0], spider[1], curses.ACS_PI)
 
 # key = KEY_RIGHT
 score = 0
 
 def get_next_move(spidy, key):
+    import pdb; pdb.set_trace()
     # get next move based on key
     if key == KEY_RIGHT:
         spidy[1] += 2
@@ -68,84 +69,93 @@ def get_next_move(spidy, key):
     else:
         return spidy
 
+class Node(object):
+    def __init__(self, state, parent, depth):
+        self.state = state
+        self.parent = parent
+        self.depth = depth
+
 def BFS(spider_state, ant_state):
     # set true when final state is true
     goal = False
-    # count the number of moves it takes
-    move_count = 0
+    node = Node(spider_state, None, 0)
     # get the initial state
-    node_list = [spider_state]
+    node_list = [node]
     # while the goal has not been reached and the nodelist isnt empty
     while goal == False or len(node_list) == 0:
         # take the first element from the node list
         e = node_list.pop(0)
-        # check if the list is empty
-        if len(node_list) == 0:
-            break
         # check every option 
+        for i in range(0, e.depth):
+            ant_state = handle_ant_movement(border_choice, ant_state)
         for key in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN]:
             # increment the move counter
-            move_count +=1
             # use the helper function to get the next move for the spider
-            next_move = get_next_move(e, key)
+            next_node = Node(None, None, None)
+            next_node.state = get_next_move(e.state, key)
+            next_node.parent = e
+            next_node.depth = e.depth + 1
             # get the next move for the ant
-            ant_state = handle_ant_movement(border_choice, ant_state)
             # if the ant and spider are the same (coordinates) the goal has been reached
-            if next_move == ant_state:
+            if next_node.state == ant_state:
                 goal = True
             else:
                 # append this state to the back of the list
-                node_list.append(e)
+                node_list.append(next_node)
     return node_list
 
+
+
+
 # random test path for the ui
-path = [[15, 12], [20, 19], [25, 25], [10, 15], [17, 23], [26, 60]]
+path = BFS(spider, ant)
+print(path)
 
-while w.getch() != 27:
-    w.timeout(50)
-    w.border(0)
-    w.addstr(0, 2, 'Score : ' + str(score) + ' ')
+# while w.getch() != 27:
+#     w.timeout(50)
+#     w.border(0)
+#     w.addstr(0, 2, 'Score : ' + str(score) + ' ')
 
-    if len(path) != 0:
-        # remove the previous spider
-        w.addch(spider[0], spider[1], ' ')
-        # remove the previous ant
-        w.addch(ant[0], ant[1], ' ')
-        # move accordingly
-        spider = path.pop(0)
+#     if len(path) != 0:
+#         # remove the previous spider
+#         w.addch(spider[0], spider[1], ' ')
+#         # remove the previous ant
+#         w.addch(ant[0], ant[1], ' ')
+#         # move accordingly
+#         spider = path.pop(0)
 
-    # if the ant goes out of bounds 
-    if ant[0] >= 30:
-        border_choice = randint(0,3)
-        ant = spawn_ant(border_choice)
-    if ant[0] < 0:
-        border_choice = randint(0,3)
-        ant = spawn_ant(border_choice)
-    if ant[1] >= 70:
-        border_choice = randint(0,3)
-        ant = spawn_ant(border_choice)
-    if ant[1] < 0:
-        border_choice = randint(0,3)
-        ant = spawn_ant(border_choice)
+#     # if the ant goes out of bounds 
+#     if ant[0] >= 30:
+#         border_choice = randint(0,3)
+#         ant = spawn_ant(border_choice)
+#     if ant[0] < 0:
+#         border_choice = randint(0,3)
+#         ant = spawn_ant(border_choice)
+#     if ant[1] >= 70:
+#         border_choice = randint(0,3)
+#         ant = spawn_ant(border_choice)
+#     if ant[1] < 0:
+#         border_choice = randint(0,3)
+#         ant = spawn_ant(border_choice)
 
-    # if the spider goes out of bounds, it comes out the other side
-    if spider[0] >= 30:
-        spider[0] = 1
-    if spider[0] < 0:
-        spider[0] = 29
-    if spider[1] >= 70:
-        spider[1] = 1
-    if spider[1] < 0:
-        spider[1] = 69
+#     # if the spider goes out of bounds, it comes out the other side
+#     if spider[0] >= 30:
+#         spider[0] = 1
+#     if spider[0] < 0:
+#         spider[0] = 29
+#     if spider[1] >= 70:
+#         spider[1] = 1
+#     if spider[1] < 0:
+#         spider[1] = 69
 
-    # check if the spider eats the ant 
-    if spider == ant:
-        score += 1
-        border_choice = randint(0,3)
-        ant = spawn_ant(border_choice)
+#     # check if the spider eats the ant 
+#     if spider == ant:
+#         score += 1
+#         border_choice = randint(0,3)
+#         ant = spawn_ant(border_choice)
 
-    # draw ant again
-    w.addch(ant[0], ant[1], curses.ACS_DIAMOND)
-     # draw spider again
-    w.addch(spider[0], spider[1], curses.ACS_PI)
+#     # draw ant again
+#     w.addch(ant[0], ant[1], curses.ACS_DIAMOND)
+#      # draw spider again
+#     w.addch(spider[0], spider[1], curses.ACS_PI)
 
