2b68e1cd8e9b03250f6aa2f9279ad9177311323f
Owen Craston <owen.craston@shopify.com>
2019-04-20 01:05:57 -0400

    bring method into class

--- course_scheduler/schedule.py
+++ course_scheduler/schedule.py
@@ -9,12 +9,20 @@ class Schedule:
     def __init__(self, courses):
         self.courses = courses
         self.course_count = len(courses)
-        self.time_table = __get_timetable__(self.courses)
+        self.time_table = self.generate_timetable()
 
     def add_course(self, course):
         self.courses.append(course)
         self.course_count = len(self.courses)
-        self.time_table =  __get_timetable__(self.courses)
+        self.time_table =  self.generate_timetable()
+    
+    def generate_timetable(self):
+        if len(self.courses):
+            time_table = {"mon":[], "tue":[], "wed":[], "thu":[], "fri":[]}
+            for c in self.courses:
+                for day in c.days:
+                    time_table[day].append(c)
+            return time_table
 
     def sort(self):
         sorted_courses = sorted(self.courses, key=lambda c: __parse_time__(c["start_time"]))
@@ -34,15 +42,6 @@ class Schedule:
         for course in self.courses:
             course.print_course()
 
-
-
-def __get_timetable__(course_list):
-    if len(course_list):
-        time_table = {"mon":[], "tue":[], "wed":[], "thu":[], "fri":[]}
-        for c in course_list:
-            for day in c.days:
-                time_table[day].append(c)
-        return time_table
 
 def __parse_time__(time_string):
         t = time.strptime(time_string, '%H:%M')
