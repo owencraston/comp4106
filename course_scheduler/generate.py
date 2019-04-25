54adfd284f2ad019fba438dab8961a1607868cf0
Owen Craston <owen.craston@shopify.com>
2019-04-24 21:14:21 -0400

    estimate total wait time per week

--- course_scheduler/generate.py
+++ course_scheduler/generate.py
@@ -93,6 +93,29 @@ def remove_time_collisions(schedule, potential_courses):
         potential_courses.remove(course)
     return potential_courses
 
+def __guess_classes_per_day__(course_count, days_off):
+    day_with_courses = 5-days_off
+    total_number_of_classes_to_attend_a_week = course_count * 2
+    classes_per_day = [0]*day_with_courses
+    total_class_list  = [1]*total_number_of_classes_to_attend_a_week
+
+    day = 0
+    for i in total_class_list:
+        classes_per_day[day] += i
+        if day == len(classes_per_day)-1:
+            day = 0
+        day += 1
+    return classes_per_day
+    
+
+def guess_total_wait_time(course_count, days_off, average_wait_time):
+    classes_per_day = __guess_classes_per_day__(course_count, days_off)
+    print(classes_per_day)
+    estimated_total_wait_time = 0
+    for classes in classes_per_day:
+        estimated_total_wait_time += ((classes - 1) * average_wait_time)
+    return estimated_total_wait_time
+
 
 def search(possible_courses, course_count):
     # starting state is the base schedule with all required courses
@@ -129,12 +152,60 @@ def search(possible_courses, course_count):
     # at this point we should have a full schedule adn we'll return it
     return current_node
 
-possible_classes = populate_classes()
-schedule = search(deepcopy(possible_classes), 5)
 
-schedule.print_time_table()
-print(f"shedule score {get_schedule_score(schedule)}")
-print(f"wait_time {schedule.total_wait_time}")
+def hill_climb_search(possible_courses, course_count, target_free_days, target_wait_time):
+    # starting state is the base schedule with all required courses
+    base_schedule = build_base_schedule()
+    remaining_courses = remove_duplicates(base_schedule.courses, possible_classes)
+    current_node = base_schedule
+    remaining_courses = remove_time_collisions(current_node, remaining_courses)
+
+    # check if we have a full schedule yet or there are no more options
+    while current_node.course_count < course_count and remaining_courses:
+        # this will be our next addition to the schedule
+        next_node = None
+        next_score = -100000000
+        # value for storing the max score from an added course
+        remaining_courses = remove_time_collisions(current_node, remaining_courses)
+        # get the next best addition to the schedule
+        for potential_course in remaining_courses:
+            temp_schedule = deepcopy(current_node)
+            # add the course to the temp schedule
+            added = temp_schedule.add_course(potential_course)
+            # if the course was valid and added
+            if added:
+                # get the score of the schedule with the potential course addition
+                temp_score = get_schedule_score(temp_schedule)
+                # if the new score is better than the previous keep it and that node
+                if temp_score > next_score:
+                    # keep this score to track it
+                    next_score = temp_score
+                    # keep this node
+                    next_node = deepcopy(potential_course)
+        # add the next_node as the next course in the schedule
+        current_node.add_course(next_node)
+
+
+        # we need to return if we have reached our goal, a schedule that has the desired amount of classes and meets the users goals
+        # if current_node.course_count == course_count:
+
+
+
+
+        # remove next_node from the remaining courses
+        remaining_courses = list(set(remove_time_collisions(current_node, remaining_courses)))
+    # at this point we should have a full schedule adn we'll return it
+    return current_node
+    
+
+possible_classes = populate_classes()
+schedule = search(deepcopy(possible_classes), 4)
+
+print(guess_total_wait_time(5, 0, 20))
+
+# schedule.print_time_table()
+# print(f"shedule score {get_schedule_score(schedule)}")
+# print(f"wait_time {schedule.total_wait_time}")
 
 
 
